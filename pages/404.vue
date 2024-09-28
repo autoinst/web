967e97f971262818b12590538d5d319a7ba92127
jdnjk <2394058778@qq.com>
2024-09-28 21:37:50 +0800

    更新 404.vue

--- pages/404.vue
+++ pages/404.vue
@@ -1,64 +1,11 @@
 <template>
-  <NuxtLayout>
-    <div class="main">
-      <div class="error">
-        <Logo404 v-if="error.statusCode === 404" />
-        <h1 v-else>发生错误!</h1>
-        <p>{{ error.message }}</p>
-        <div class="button-group">
-          <nuxt-link to="/" class="iconified-button raised-button brand-button">
-            重试?
-          </nuxt-link>
-          <a
-            href="https://github.com/jdnjk/autoinst_web/issues"
-            class="iconified-button raised-button"
-            rel="noopener"
-          >
-            出错了?在Github取得帮助
-          </a>
-        </div>
-      </div>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <p class="my-8 text-xl text-gray-800">好欸，是404 </p>
     </div>
-  </NuxtLayout>
+  </div>
 </template>
 
 <script setup>
-import Logo404 from "../assets/images/404.svg";
-
-defineProps({
-  error: {
-    type: Object,
-    default() {
-      return {
-        statusCode: 1000,
-        message: "Unknown error",
-      };
-    },
-  },
-});
+setResponseStatus(404)
 </script>
-
-<style lang="scss" scoped>
-.main {
-  margin: var(--spacing-card-lg) auto;
-  width: calc(100% - 4rem);
-
-  @media screen and (min-width: 800px) {
-    width: 600px;
-  }
-}
-
-.error {
-  h1 {
-    margin-bottom: 0.25rem;
-  }
-
-  svg {
-    fill: var(--color-text);
-    color: var(--color-text);
-
-    width: 100%;
-    height: auto;
-  }
-}
-</style>
