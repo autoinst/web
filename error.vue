231a9ed96d412082d196669a7f9b610f2b540935
jdnjk <2394058778@qq.com>
2024-10-03 21:18:06 +0800

    我c

---
+++ error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="error-page">
+    <el-empty description="404 - 页面未找到">
+      <el-button type="primary" @click="goHome">返回首页</el-button>
+    </el-empty>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+}
+
+.el-empty {
+  font-size: 18px;
+}
+</style>
